ded571a863d90734f004cc630f0dde70afd3e4cb
Silva Charles <silvacharlesniyokindi@gmail.com>
2026-06-25 21:19:15 +0300

    created Header.jsx; and used it in homepage and orders page

--- ecommerce/src/pages/Homepage.jsx
+++ ecommerce/src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import "./Header.css";
+import { Header } from "./Header";
 // import './App.css'
 import "./HomePage.css";
 
@@ -7,47 +7,8 @@ export function HomePage() {
     <>
       <title>Homepage</title>
 
-      <div class="header">
-        <div class="left-section">
-          <a href="/" class="header-link">
-            <img alt="image" class="logo" src="images/logo-white.png" />
-            <img
-              alt="image"
-              class="mobile-logo"
-              src="images/mobile-logo-white.png"
-            />
-          </a>
-        </div>
-
-        <div class="middle-section">
-          <input class="search-bar" type="text" placeholder="Search" />
-
-          <button class="search-button">
-            <img
-              alt="image"
-              class="search-icon"
-              src="images/icons/search-icon.png"
-            />
-          </button>
-        </div>
-
-        <div class="right-section">
-          <a class="orders-link header-link" href="/orders">
-            <span class="orders-text">Orders</span>
-          </a>
-
-          <a class="cart-link header-link" href="/checkout">
-            <img
-              alt="image"
-              class="cart-icon"
-              src="images/icons/cart-icon.png"
-            />
-            <div class="cart-quantity">3</div>
-            <div class="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
-
+      <Header />
+      
       <div class="home-page">
         <div class="products-grid">
           <div class="product-container">
