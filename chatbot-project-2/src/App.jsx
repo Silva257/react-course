c10115977eb97cbee786c3cc6ed3224157b3b0c3
Silva Charles <silvacharlesniyokindi@gmail.com>
2026-06-29 21:18:34 +0300

    created the ChatMessages components

--- chatbot-project-2/src/App.jsx
+++ chatbot-project-2/src/App.jsx
@@ -1,9 +1,15 @@
 import ChatInput from "./components/ChatInput";
+import ChatMessages from "./components/ChatMessages";
 
 function App() {
   return (
     <>
       <h1>chatbot app</h1>
+      
+      <ChatMessages message="hello now" sender="user" />
+
+      <ChatMessages message="hello how can i help you" sender="robot" />
+
       <ChatInput />
     </>
   );
---
+++ chatbot-project-2/src/components/ChatMessages.jsx
@@ -0,0 +1,17 @@
+import userImage from "../assets/user.png";
+import robotImage from "../assets/robot.png";
+
+function ChatMessages({ message, sender }) {
+  const user = <img src={userImage} alt="user" width="50" />;
+  const robot = <img src={robotImage} alt="user" width="50" />;
+
+  return (
+    <div>
+      {sender === "user" && user}
+      {message}
+      {sender === "robot" && robot}
+    </div>
+  );
+}
+
+export default ChatMessages;
